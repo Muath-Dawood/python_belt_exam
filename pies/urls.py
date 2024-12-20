b33ebae376076c602dde6880176a930ce026857f
Muath Dawood <muath.dawood.89@gmail.com>
2024-12-20 15:07:58 +0200

    setup index route

--- pies/urls.py
+++ pies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import dashboard, list_pies, show_pie, edit_pie, delete_pie
+from .views import index, dashboard, list_pies, show_pie, edit_pie, delete_pie
 
 urlpatterns = [
+    path('', index, name='pies_index'),
     path('dashboard/', dashboard, name='pies_dashboard'),
     path('pies/',list_pies, name='pies_list_pies'),
     path('pies/<int:id>/', show_pie, name='pies_show_pie'),
--- pies/views.py
+++ pies/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+def index(req):
+  if req.user.is_authenticated:
+    return redirect('/dashboard')
+  else:
+    return redirect('/accounts/signin')
 
 def dashboard(req):
   return render(req, 'pies/dashboard.html')
