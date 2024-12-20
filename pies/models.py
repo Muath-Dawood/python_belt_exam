f9a282f2a8fe9ec5b9a583f80680a553ee4c4139
Muath Dawood <muath.dawood.89@gmail.com>
2024-12-20 15:00:02 +0200

    migrate pies models

--- pies/models.py
+++ pies/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Pie(models.Model):
-  name = models.CharField(blank=False, unique=True)
-  filling = models.CharField(blank=False)
-  crust = models.CharField(blank=False)
+  name = models.CharField(blank=False, unique=True, max_length=255)
+  filling = models.CharField(blank=False, max_length=255)
+  crust = models.CharField(blank=False, max_length=255)
 
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pies')
--- pies/urls.py
+++ pies/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('dashboard/', dashboard, name='pies_dashboard'),
     path('pies/',list_pies, name='pies_list_pies'),
     path('pies/<int:id>/', show_pie, name='pies_show_pie'),
-    path('pies/edit/<int:id/', edit_pie, name='pies_edit_pie'),
-    path('pies/delete/<int:id/', delete_pie, name='pies_delete_pie'),
+    path('pies/edit/<int:id>/', edit_pie, name='pies_edit_pie'),
+    path('pies/delete/<int:id>/', delete_pie, name='pies_delete_pie'),
 ]
